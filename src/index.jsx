4c0476d9cf1340cf0ca315c82d70b0605660e7ae
Chernyshov <xpom55@mail.ru>
2018-07-19 22:34:04 +0300

    run App simple app

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,19 @@
-console.log('Hello JSX 22');
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+
+render(<App />, document.getElementById('root'));
+
+
+// import React from 'react';
+// import { render } from 'react-dom';
+
+// const App = () => (
+//     <div>
+//       <h3>Our Application Is Alive</h3>
+//       <p>This isn’t reality. This — is fantasy.</p>
+//       <p>Yes I am quoting Star Trek I cant help it.</p>
+//     </div>
+//   );
+
+// render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,40 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+});
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
+    filename: 'index_bundle.js'
   },
 
   resolve: {
-    extensions: ['js', 'jsx'],
+    extensions: ['.js', '.jsx']
   },
-  plugins: [new HtmlWebpackPlugin()],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // presets: ['env', 'react', 'stage-3'],
+            // presets: ['@babel/preset-env', 'react', 'stage-3'],
+            // plugins: ['transform-runtime'],
+            // include: ['transform-es2015-arrow-functions', 'es6.map'],
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [htmlWebpackPlugin]
 };
